35cd0da51871b151011f2cd128a5f97ab3c70451
Nahuel <nahuelarancibia4@gmail.com>
2023-03-30 19:46:28 -0300

    componentes

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { QualificationsComponent } from './components/qualifications/qualifications.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    ProyectosComponent,
+    TecnologiasComponent,
+    QualificationsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
